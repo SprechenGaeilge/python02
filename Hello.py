af89634cab4fa2a1a084acf5990556d43a51b470
SprechenGaeilge <130784669+SprechenGaeilge@users.noreply.github.com>
2023-04-14 19:33:30 +0100

    hello.py

---
+++ Hello.py
@@ -0,0 +1,13 @@
+
+import winsound
+frequency = 250  # Set Frequency To 2500 Hertz
+duration = 1000  # Set Duration To 1000 ms == 1 second
+
+
+print("Read Me")
+def hello():
+    print("Hello")
+    print('/a')
+    winsound.Beep(frequency, duration)
+hello()
+
